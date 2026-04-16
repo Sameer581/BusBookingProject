ba97d4d81abbd51cba9c7a96c275385b6aa1d227
Zaid <kzaid2144@gmail.com>
2026-04-16 13:05:04 +0530

    changed int to long in id

--- src/main/java/com/cg/entity/BusBooking.java
+++ src/main/java/com/cg/entity/BusBooking.java
@@ -1,4 +1,5 @@
 package com.cg.entity;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -14,27 +15,84 @@ import jakarta.persistence.OneToMany;
 
 @Entity
 public class BusBooking {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Column(name = "booking_dt")
+	private LocalDate bookingDate;
 
-    @Column(name = "booking_dt")
-    private LocalDate bookingDate;
-    
-    @Column(name = "booking_status")
-    private String bookingStatus;
+	@Column(name = "booking_status")
+	private String bookingStatus;
 
-    @ManyToOne
-    @JoinColumn(name = "schedule_id")
-    private RouteSchedule schedule;
+	@ManyToOne
+	@JoinColumn(name = "schedule_id")
+	private RouteSchedule schedule;
 
-    @ManyToOne
-    @JoinColumn(name = "cust_id")
-    private Customer customer;
+	@ManyToOne
+	@JoinColumn(name = "cust_id")
+	private Customer customer;
 
-    @OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
-    private List<Passenger> passengers;
+	@OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
+	private List<Passenger> passengers;
 
-    // getters & setters
+	public BusBooking() {
+	}
+
+	public BusBooking(Long id, LocalDate bookingDate, String bookingStatus, RouteSchedule schedule, Customer customer) {
+		super();
+		this.id = id;
+		this.bookingDate = bookingDate;
+		this.bookingStatus = bookingStatus;
+		this.schedule = schedule;
+		this.customer = customer;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getBookingDate() {
+		return bookingDate;
+	}
+
+	public void setBookingDate(LocalDate bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+
+	public String getBookingStatus() {
+		return bookingStatus;
+	}
+
+	public void setBookingStatus(String bookingStatus) {
+		this.bookingStatus = bookingStatus;
+	}
+
+	public RouteSchedule getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(RouteSchedule schedule) {
+		this.schedule = schedule;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<Passenger> getPassengers() {
+		return passengers;
+	}
+
+	public void setPassengers(List<Passenger> passengers) {
+		this.passengers = passengers;
+	}
 }
