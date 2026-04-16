a0f376869d18c6659e92170726141cafe672ec13
Zaid <kzaid2144@gmail.com>
2026-04-16 15:05:25 +0530

    service fix

--- src/main/java/com/cg/service/BusBookingService.java
+++ src/main/java/com/cg/service/BusBookingService.java
@@ -5,10 +5,11 @@ import java.util.List;
 import com.cg.dto.BusBookingDto;
 import com.cg.dto.RouteScheduleDto;
 import com.cg.entity.BusBooking;
+import com.cg.entity.RouteSchedule;
 
 public interface BusBookingService {
 
-    public RouteScheduleDto createSchedule(RouteScheduleDto dto);
+    public RouteSchedule createSchedule(RouteScheduleDto dto);
     public BusBooking createBooking(BusBookingDto dto);
     public List<BusBookingDto> getAllBookings();
     public List<BusBookingDto> getBookingsByCustomer(Long custId);
