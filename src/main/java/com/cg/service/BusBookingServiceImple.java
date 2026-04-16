c36be037ffc7ce03873cf6f57ae870f6fb126fce
Zaid <kzaid2144@gmail.com>
2026-04-16 15:21:56 +0530

    conflict resolve

--- src/main/java/com/cg/service/BusBookingServiceImple.java
+++ src/main/java/com/cg/service/BusBookingServiceImple.java
@@ -2,29 +2,76 @@ package com.cg.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cg.dto.BookingToDto;
 import com.cg.dto.BusBookingDto;
 import com.cg.dto.RouteScheduleDto;
 import com.cg.entity.BusBooking;
+import com.cg.entity.BusRoute;
+import com.cg.entity.Customer;
 import com.cg.entity.RouteSchedule;
+import com.cg.exception.NotAvailableException;
+import com.cg.repo.BusBookingRepo;
+import com.cg.repo.BusRouteRepo;
+import com.cg.repo.CustomerRepo;
+import com.cg.repo.RouteScheduleRepo;
 
-@Service
-public class BusBookingServiceImple implements BusBookingService{
+public class BusBookingServiceImple implements BusBookingService {
+
+	@Autowired
+	private RouteScheduleRepo scheduleRepo;
+
+	@Autowired
+	private BusRouteRepo routeRepo;
+
+	@Autowired
+	private CustomerRepo custRepo;
+
+	@Autowired
+	private BusBookingRepo bookingRepo;
 
 	@Override
 	public RouteSchedule createSchedule(RouteScheduleDto dto) {
-		return null;
+		RouteSchedule schedule = new RouteSchedule();
+		BusRoute route = routeRepo.findById(dto.routeId())
+				.orElseThrow(() -> new NotAvailableException("bus route not found with id " + dto.routeId()));
+
+		schedule.setAvailableSeats(dto.availableSeats());
+		schedule.setDepartureTime(dto.departureTime());
+		schedule.setScheduleDate(dto.scheduleDate());
+		schedule.setRoute(route);
+		schedule.setTotalSeats(dto.totalSeats());
+		schedule.setScheduleStatus("SCHEDULED");
+
+		return scheduleRepo.save(schedule);
 	}
 
 	@Override
 	public BusBooking createBooking(BusBookingDto dto) {
 
-		return null;
+		BusBooking booking = new BusBooking();
+		Customer cust = custRepo.findById(dto.custId())
+				.orElseThrow(() -> new NotAvailableException("customer not found with id " + dto.custId()));
+		RouteSchedule schedule = scheduleRepo.findById(dto.scheduleId())
+				.orElseThrow(() -> new NotAvailableException("route schedule not found with id " + dto.scheduleId()));
+
+		booking.setBookingDate(dto.bookingDt());
+		booking.setCustomer(cust);
+		booking.setSchedule(schedule);
+		booking.setBookingStatus("BOOKED");
+		return bookingRepo.save(booking);
 	}
 
 	@Override
 	public List<BusBookingDto> getBookingsByCustomer(Long custId) {
-		
-		return null;
+
+		Customer cust = custRepo.findById(custId)
+				.orElseThrow(() -> new NotAvailableException("customer not found with id " + custId));
+
+		List<BusBookingDto> bookings = cust.getBookings().stream().map(BookingToDto::mapToDto).toList();
+
+		return bookings;
 	}
 
 }
