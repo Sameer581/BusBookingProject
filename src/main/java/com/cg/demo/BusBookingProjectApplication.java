10670978ef6c4cc56c4241428547fceefb015bf0
Sameer <sameersid9897@gmail.com>
2026-04-16 12:32:22 +0530

    Initial push

---
+++ src/main/java/com/cg/demo/BusBookingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingProjectApplication.class, args);
+	}
+
+}
