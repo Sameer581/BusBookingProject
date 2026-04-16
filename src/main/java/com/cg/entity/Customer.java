a0ee3576cab8bc6e03ee39afa7f7d1f8bb715c2b
Sameer <sameersid9897@gmail.com>
2026-04-16 12:53:29 +0530

    Nice

--- src/main/java/com/cg/entity/Customer.java
+++ src/main/java/com/cg/entity/Customer.java
@@ -1,5 +1,51 @@
 package com.cg.entity;
 
-public class Customer {
+import org.hibernate.annotations.DynamicUpdate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@DynamicUpdate
+@Table(name="customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="cust_id")
+	private Integer customerId;
+	
+	@Column(name="cust_name")
+	private String customerName;
+	
+	@Column(name="cust_phone_no")
+	private Integer customerPhoneNumber;
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Integer getCustomerPhoneNumber() {
+		return customerPhoneNumber;
+	}
+
+	public void setCustomerPhoneNumber(Integer customerPhoneNumber) {
+		this.customerPhoneNumber = customerPhoneNumber;
+	}
+	
+	
 }
