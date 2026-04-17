6b2827199ce1e2e8f4d010984e6e89cecb6178c8
Zaid <kzaid2144@gmail.com>
2026-04-17 12:51:25 +0530

    added securityconfig and authfilter

--- src/main/java/com/cg/config/SecurityConfig.java
+++ src/main/java/com/cg/config/SecurityConfig.java
@@ -1,5 +1,51 @@
 package com.cg.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.cg.security.JWTAuthFilter;
+
+@Configuration
 public class SecurityConfig {
 
+	@Autowired
+	private JWTAuthFilter jwtAuthFilter;
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:4200"));
+		config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/generateToken").permitAll())
+//        		
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/swagger-ui.html").permitAll())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/booking/**").authenticated())
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/schedule/**").authenticated())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/addcart","/editcart").hasRole("USER"))
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/addproduct").hasRole("ADMIN"))
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class).build();
+	}
 }
