5305a87b7bc9f357db9857b0d733d93fdde44f66
Zaid <kzaid2144@gmail.com>
2026-04-17 15:25:15 +0530

    login-component.ts done

--- Angular/bus-booking/src/app/login/login.component.ts
+++ Angular/bus-booking/src/app/login/login.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookingService } from '../booking.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  frm: FormGroup;
 
+  res: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: BookingService,
+    private router: Router,
+  ) {
+    this.frm = this.fb.group({
+      uname: ['', Validators.required],
+      pwd: ['', Validators.required],
+    });
+  }
+
+  doLogin() {
+    console.log(this.frm.value);
+    let uname = this.frm.value.uname;
+    let pwd = this.frm.value.pwd;
+    console.log(uname, pwd);
+    this.auth.doLogin({ username: uname, password: pwd }).subscribe({
+      next: (response: any) => {
+        this.res = true;
+        let token = response.token;
+        console.log('Login successful, token:', response, token);
+        this.auth.token = token;
+        this.auth.username = response.username;
+        this.router.navigate(['/schedule']);
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+        this.res = false;
+      },
+    });
+  }
 }
