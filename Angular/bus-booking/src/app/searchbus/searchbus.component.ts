a6a1770a1d2048113532ae7d446b538592c250de
Sameer <sameersid9897@gmail.com>
2026-04-20 14:59:00 +0530

    error solved

--- Angular/bus-booking/src/app/searchbus/searchbus.component.ts
+++ Angular/bus-booking/src/app/searchbus/searchbus.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { CommonModule} from '@angular/common';g
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-searchbus',
